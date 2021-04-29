d1bef53b3a9c8895cf73263f7453e0d1bc79619d
sinhro200 <sinhro-200@mail.ru>
2021-04-29 23:47:42 +0300

    обновил версию либы отрисовывающей боковое меню тулбар сделал полностью кастомным, со своими иконками гамбургера и стрелки влево сменил главную тему приложения

--- Frontend/app/src/main/java/org/vsu/pt/team2/utilitatemmetrisapp/ui/components/DrawerController.kt
+++ Frontend/app/src/main/java/org/vsu/pt/team2/utilitatemmetrisapp/ui/components/DrawerController.kt
@@ -1,118 +1,203 @@
 package org.vsu.pt.team2.utilitatemmetrisapp.ui.components
 
-import android.util.Log
 import android.view.View
-import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
 import androidx.drawerlayout.widget.DrawerLayout
-import com.mikepenz.materialdrawer.AccountHeader
-import com.mikepenz.materialdrawer.AccountHeaderBuilder
-import com.mikepenz.materialdrawer.Drawer
-import com.mikepenz.materialdrawer.DrawerBuilder
+import com.mikepenz.materialdrawer.holder.ImageHolder
+import com.mikepenz.materialdrawer.holder.StringHolder
+import com.mikepenz.materialdrawer.model.DividerDrawerItem
 import com.mikepenz.materialdrawer.model.PrimaryDrawerItem
 import com.mikepenz.materialdrawer.model.ProfileDrawerItem
 import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem
-import com.orhanobut.logger.Logger
+import com.mikepenz.materialdrawer.model.interfaces.descriptionText
+import com.mikepenz.materialdrawer.model.interfaces.nameText
+import com.mikepenz.materialdrawer.util.addItems
+import com.mikepenz.materialdrawer.util.addStickyDrawerItems
+import com.mikepenz.materialdrawer.widget.AccountHeaderView
+import com.mikepenz.materialdrawer.widget.MaterialDrawerSliderView
 import org.vsu.pt.team2.utilitatemmetrisapp.R
+import org.vsu.pt.team2.utilitatemmetrisapp.databinding.AppbarContentBinding
 import org.vsu.pt.team2.utilitatemmetrisapp.ui.main.SettingsFragment
 import org.vsu.pt.team2.utilitatemmetrisapp.ui.tools.replaceFragment
 
 class DrawerController(
     private val activity: AppCompatActivity,
-    private val toolbar: Toolbar
+    private val materialDrawerSliderView: MaterialDrawerSliderView,
+    private val appbarContentBinding: AppbarContentBinding
+
 ) {
-    private lateinit var mDrawer: Drawer
-    private lateinit var mHeader: AccountHeader
-    private lateinit var mDrawerLayout: DrawerLayout
+    private var mDrawerLayout: DrawerLayout? = null
 
     fun create() {
         createHeader()
         createDrawer()
-        mDrawerLayout = mDrawer.drawerLayout
+        mDrawerLayout = materialDrawerSliderView.drawerLayout
     }
 
     fun disableDrawer() {
-        mDrawer.actionBarDrawerToggle?.isDrawerIndicatorEnabled = false
-        activity.supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        mDrawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED)
-        toolbar.setNavigationOnClickListener {
+        appbarContentBinding.iconHamburger.visibility = View.GONE
+        appbarContentBinding.iconBack.visibility = View.VISIBLE
+//        mDrawer.actionBarDrawerToggle?.isDrawerIndicatorEnabled = false
+        mDrawerLayout?.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED)
+        appbarContentBinding.toolbarIconContainer.setOnClickListener {
             activity.supportFragmentManager.popBackStack()
         }
     }
 
     fun enableDrawer() {
-        activity.supportActionBar?.setDisplayHomeAsUpEnabled(false)
-        mDrawer.actionBarDrawerToggle?.isDrawerIndicatorEnabled = true
-        mDrawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED)
-        toolbar.setNavigationOnClickListener {
-            Logger.i("Navigation clicked")
-            mDrawer.openDrawer()
+        appbarContentBinding.iconHamburger.visibility = View.VISIBLE
+        appbarContentBinding.iconBack.visibility = View.GONE
+//        mDrawer.actionBarDrawerToggle?.isDrawerIndicatorEnabled = false
+        mDrawerLayout?.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED)
+        appbarContentBinding.toolbarIconContainer.setOnClickListener {
+            materialDrawerSliderView.drawerLayout?.open()
+        }
+    }
+
+    fun initAnonimous(): DrawerItemsEasyCreator {
+        return DrawerItemsEasyCreator().apply {
+            addItem(
+                simpleMenuItem("~Оплатить"),
+                { view, pos, drItem ->
+                    //todo
+                }
+            )
+            addItem(
+                simpleMenuItem("~Сохранённые счётчики"),
+                { view, pos, drItem ->
+                    //todo
+                }
+            )
+            addItem(
+                DividerDrawerItem()
+            )
+            addItem(
+                simpleMenuItem("~Продолжить регистрацию"),
+                { view, pos, drItem ->
+                    //todo
+                }
+            )
+        }
+    }
+
+    fun initDefalut(): DrawerItemsEasyCreator {
+        return DrawerItemsEasyCreator().apply {
+            addItem(
+                simpleMenuItem("~Оплатить"),
+                { view, pos, drItem ->
+                    //todo
+                }
+            )
+            addItem(
+                simpleMenuItem("~Сохранённые счётчики"),
+                { view, pos, drItem ->
+                    //todo
+                }
+            )
+            addItem(
+                DividerDrawerItem()
+            )
+            addItem(
+                simpleMenuItem("~Добавить счёт"),
+                { view, pos, drItem ->
+                    //todo
+                }
+            )
+            addItem(
+                simpleMenuItem("~Мои счета"),
+                { view, pos, drItem ->
+                    //todo
+                }
+            )
+            addItem(
+                DividerDrawerItem()
+            )
+            addItem(
+                simpleMenuItem("~История выплат"),
+                { view, pos, drItem ->
+                    //todo
+                }
+            )
         }
     }
 
     private fun createDrawer() {
-        mDrawer = DrawerBuilder()
-            .withActivity(activity)
-            .withToolbar(toolbar)
-            .withActionBarDrawerToggle(true)
-            .withSelectedItem(-1)
-            .withAccountHeader(mHeader)
-            .addDrawerItems(
-                PrimaryDrawerItem()
-                    .withIdentifier(10)
-                    .withIconTintingEnabled(true)
-                    .withName("123")
-                    .withSelectable(false),
-            )
-            .addStickyDrawerItems(
-                PrimaryDrawerItem()
-                    .withIdentifier(1010)
-                    .withIconTintingEnabled(true)
-                    .withName("Настройки")
-                    .withSelectable(false)
-            )
-            .withOnDrawerItemClickListener(object : Drawer.OnDrawerItemClickListener {
-                override fun onItemClick(
-                    view: View?,
-                    position: Int,
-                    drawerItem: IDrawerItem<*>
-                ): Boolean {
-                    when (drawerItem.identifier) {
-                        1010L ->
-                            activity.replaceFragment(SettingsFragment())
-//                        10L ->
-//                            activity.replaceFragment(CreateRoomFragment())
-                    }
-                    return false
-                }
-            }).build()
+        val drawerItemsEasyCreator = initDefalut()//initAnonimous()
+        for (item in drawerItemsEasyCreator.items()) {
+            materialDrawerSliderView.addItems(item)
+        }
+
+        materialDrawerSliderView.addStickyDrawerItems(
+            simpleMenuItem("~Настройки").apply { identifier = 1010 }
+        )
+        materialDrawerSliderView.onDrawerItemClickListener = { view: View?,
+                                                               drawerItem: IDrawerItem<*>,
+                                                               position: Int ->
+            if (drawerItem.identifier == 1010L)
+                activity.replaceFragment(SettingsFragment())
+            else
+                drawerItemsEasyCreator.clickListener().invoke(view, drawerItem, position)
+            false
+        }
     }
 
     private fun createHeader() {
-        mHeader = AccountHeaderBuilder()
-            .withActivity(activity)
-            .withProfiles(
-                mutableListOf(
-                    ProfileDrawerItem()
-                        .withName("name")
-                        .withEmail("email")
-                        .withNameShown(true)
-                )
-            )
-//            .withHeaderBackground(R.drawable.header)
-            .addProfiles(
+        val profileItem = ProfileDrawerItem().apply {
+            nameText = "name"
+            descriptionText = "email"
+        }
+        AccountHeaderView(activity).apply {
+            attachToSliderView(materialDrawerSliderView)
+            profiles?.add(profileItem)
+            selectionListEnabledForSingleProfile = false
+            headerBackground = ImageHolder(R.drawable.header)
 
+        }
+    }
+
+    private fun simpleMenuItem(text: String): IDrawerItem<*> {
+        return PrimaryDrawerItem().apply {
+            isIconTinted = true
+            name = StringHolder(text)
+            isSelectable = false
+        }
+    }
+
+    class DrawerItemsEasyCreator {
+        private var identifierCounter = 0;
+
+        private val drawerItemIdToPairItemAction = mutableMapOf<Int, Pair<IDrawerItem<*>, ((
+            view: View?,
+            drawerItem: IDrawerItem<*>,
+            position: Int,
+        ) -> Unit)?>>()
+
+        fun addItem(
+            item: IDrawerItem<*>,
+            onClick: ((
+                view: View?,
+                drawerItem: IDrawerItem<*>,
+                position: Int,
+            ) -> Unit)? = null
+        ) {
+            drawerItemIdToPairItemAction[identifierCounter] = Pair(
+                item.apply { identifier = identifierCounter.toLong() }, onClick
             )
-            .build()
-//        activity.viewModels<UserInfoViewModel>().value.userLiveData.observe(
-//            activity, {
-//                mHeader.updateProfile(
-//                    ProfileDrawerItem()
-//                        .withName(it.nickname)
-//                        .withEmail(it.email)
-//                )
-//            }
-//        )
+            identifierCounter++
+        }
+
+        fun items(): List<IDrawerItem<*>> {
+            return drawerItemIdToPairItemAction.values.map { it.first }
+        }
+//        fun addItemsToDrawer(mdsv: MaterialDrawerSliderView) {
+//            for (itemAndAction in drawerItemIdToPairItemAction.values)
+//                mdsv.addItems(itemAndAction.first)
+//        }
+
+        fun clickListener(): ((v: View?, item: IDrawerItem<*>, position: Int) -> Boolean) =
+            { v: View?, item: IDrawerItem<*>, position: Int ->
+                drawerItemIdToPairItemAction[item.identifier]?.second?.invoke(v, item, position)
+                false
+            }
     }
 }
